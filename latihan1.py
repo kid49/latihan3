d61512f84bf8c9a1d02712623c24f2f0e29ce3ef
kid49 <marcoroland92@yahoo.com>
2019-02-21 17:50:18 +0700

    nambah gambar

--- latihan1.py
+++ latihan1.py
@@ -8,4 +8,12 @@ for i in range(N):
 	N = random.uniform(0.0,0.5)
 	print('Data ke : ',a, '==>' ,N)
 print("---SELESAI---")
-
+print ('')
+jawab = "lanjutkan"
+hitung = 0
+while jawab == "lanjutkan " :
+	hitung +=1
+	if jawab == 3:
+		break
+	jawab = input("Ingin mengulang? (lanjut/tidak) ?")
+print("Total perulangan  :" + str(hitung))
